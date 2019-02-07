cf41cf962915d692168a4838442d49a8d340012f
Asif Ahmed <32269425+AsifAhmed85@users.noreply.github.com>
2019-02-08 05:55:04 +0600

    update
    dummy update

--- hello.java
+++ hello.java
@@ -3,13 +3,13 @@ public class hello{
   public static void main(String args[])
   {
       System.out.println("Hello World");
-      System.out.println("New Line");
+      System.out.println("New Line updated");
 
       System.out.println("Extra code in new brach");
 
       for(int i = 0; i < 10; i++)
       {
-          System.out.println(i*10);
+          System.out.println(i*12);
       }
 
   }
